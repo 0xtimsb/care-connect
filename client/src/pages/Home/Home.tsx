21bc87379837cb9972b2c3cf39662f02d18e010c
Smit Barmase <smitbarmase@outlook.com>
2020-10-10 02:58:25 +0530

    Chart added.

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Chart from 'components/Chart/Chart';
+
 const Root = styled.div`
 	width: 100%;
 	height: 100vh;
 
 	display: flex;
 	justify-content: center;
-	align-items: center;
+	align-items: flex-end;
 `;
 
 const Home = ({ userData }: any) => {
-	return <Root>'Home (You are logged in!)'</Root>;
+	return (
+		<Root>
+			<Chart />
+		</Root>
+	);
 };
 
 export default Home;
