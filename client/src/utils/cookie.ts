8b7d073f3bdf6147d1b7e5e90b6b04ce1738f3af
Smit Barmase <smitbarmase@outlook.com>
2020-10-10 12:00:28 +0530

    Client Update

--- client/src/utils/cookie.ts
+++ client/src/utils/cookie.ts
@@ -1,23 +1,27 @@
 export const setCookie = (name: string, value: string) => {
-  const date = new Date();
+  let expires = "";
   const days = 3;
-  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + date.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  console.log(value);
+  console.log(name + "=" + (value || "") + expires + "; path=/");
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 };
 
 export const getCookie = (name: string) => {
-  name = name + "=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
-  return "";
+  return null;
+};
+
+export const eraseCookie = (name: string) => {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
