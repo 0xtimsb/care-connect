2da799bbd435a288bbf9212243caa7ac031f147e
Smit Barmase <smitbarmase@outlook.com>
2020-10-11 03:51:14 +0530

    Root directory heroku

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ const sendData = async (socket: Socket) => {
 app.use(userRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../../client/build'));
+  app.use(express.static('./client/build'));
 }
 
 // Error handling
